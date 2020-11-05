e6944aae915eeb8ca41346d0f26b03f7282eeda2
n3m1.sys <mromera99@gmail.com>
2020-11-05 22:37:21 +0100

    Script para resolver el ejercicio 1

---
+++ Ejercicios/ejercicio1.R
@@ -0,0 +1,36 @@
+# Author: Mart??n Romera Sobrado
+# 
+# Exercises for "Data analysis recipes: Fitting a model to data" 
+#
+# Fitting a straight line to data - Exercise 1
+
+library(matlib)
+setwd(getSrcDirectory()[1])
+# Cargamos los datos de la tabla y eliminamos los datos sobrantes
+table <- read.csv('Datos/tabla1.csv')
+table = table[5:nrow(table),1:4]
+
+# Recogemos los datos de table en variables diferentes
+x <- table[,"x"]
+y <- table[,"y"]
+sigmay <- table[,"sigma_y"]
+
+# Creamos las matrices para el calulo de la matriz X
+YMat <- matrix(y, ncol = 1)
+AMat <- matrix(1, nrow = length(x), ncol = 2)
+AMat[,2] = x 
+CMat <- diag(x = sigmay)
+
+# Calculamos la matriz X
+XMat <- inv(t(AMat)%*%inv(CMat)%*%AMat)%*%(t(AMat)%*%inv(CMat)%*%YMat)
+line = function(x){XMat[2]*x + XMat[1]}
+# Mostramos datos
+plot(x,y,
+     ylim=range(c(y-sigmay, y+sigmay)), 
+     pch=19, xlab="X", ylab="Y", 
+     main="Plotted Data"
+)
+abline(a = XMat[1], b = XMat[2])
+arrows(x,y-sigmay,x,y+sigmay,length=0.05,angle=90,code=3)
+
+
